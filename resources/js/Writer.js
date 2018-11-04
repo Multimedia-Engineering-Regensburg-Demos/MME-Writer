76e894248c070b2bd183890c7dfbc758ffd8f725
Alexander Bazo <alexanderbazo@googlemail.com>
2018-11-04 18:15:55 +0100

    Add solution

--- resources/js/Writer.js
+++ resources/js/Writer.js
@@ -5,10 +5,36 @@ var Writer = Writer || {};
 Writer.App = (function() {
   "use strict";
 
-  var that = {};
+  var that = {},
+    storage,
+    stats,
+    editor;
 
   function init() {
-    console.log("in: init() [Writer.js]");
+    initView();
+    initController();
+    initStorage();
+  }
+
+  function initView() {
+    let statsEl = document.querySelector("#stats");
+    stats = Writer.StatsView().init(statsEl);
+  }
+
+  function initController() {
+    let editorEl = document.querySelector("#editor");
+    editor = Writer.EditorController().init(editorEl);
+    editor.addEventListener("textChanged", onTextChanged);
+  }
+
+  function initStorage() {
+    storage = Writer.Storage();
+    editor.setText(storage.load());
+  }
+
+  function onTextChanged(event) {
+    stats.updateStats(event.stats);
+    storage.save(editor.getText());
   }
 
   that.init = init;
